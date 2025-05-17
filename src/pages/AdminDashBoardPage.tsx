00418ca1a2c8b34fbe85d343005b3fb4186e309c
66zvnc <ivakkkk@icloud.com>
2025-05-17 16:12:04 +0300

    toast done

--- src/pages/AdminDashBoardPage.tsx
+++ src/pages/AdminDashBoardPage.tsx
@@ -2,13 +2,20 @@ import {DataTableDemo} from "@/components/ui/shadcn/data-table.tsx";
 import {useEffect} from "react";
 import {toast} from "react-toastify";
 function AdminDashBoardPage() {
-    useEffect(()=> {
-        toast("sdfghj")
-    })
+    const notify = () => toast('Daniel Todorov walks in the door!');
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            notify();
+        }, Math.random() * Math.PI * 5);
+
+        return () => clearTimeout(timeout);
+    });
+
     return (
         <div className="mx-5">
             <h1 className="mt-2 text-3xl font-bold">Logo</h1>
             <DataTableDemo/>
+
 
         </div>
     );
