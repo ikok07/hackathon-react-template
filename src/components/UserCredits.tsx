de507f3fc80e49ce9307b581ffd9023cef1470ac
66zvnc <ivakkkk@icloud.com>
2025-05-17 11:22:20 +0300

    add dialog to coins

--- src/components/UserCredits.tsx
+++ src/components/UserCredits.tsx
@@ -1,12 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
 import {useAppUser} from "@/hooks/auth/useAppUser.ts";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui//shadcn/dialog"
+import PrimaryButton from "@/components/ui/buttons/PrimaryButton.tsx";
 
 export default function UserCredits() {
     // const userCredits= 25;
     const {userObject, dbUser} = useAppUser();
-    const {data: credits, isLoading, error}= useQuery({
-        queryFn: async()=>{
+    const {data: credits, isLoading, error} = useQuery({
+        queryFn: async () => {
             const {data} = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/profile-balances/${userObject.user!.id}`, {
                 headers: {
                     Authorization: import.meta.env.VITE_BACKEND_API_KEY!
@@ -19,10 +28,24 @@ export default function UserCredits() {
         enabled: !!userObject.user
     })
     return (
-        <div>
-            <button className="flex gap-1">
-                <p>{credits?.amount_credits ?? 0}</p> <img src="/credits.svg" alt='credits' width={20} height={20} />
-            </button>
-        </div>
+        <>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <button className="flex gap-1">
+                        <p>{credits?.amount_credits ?? 0}</p> <img src="/credits.svg" alt='credits' width={20} height={20}/>
+                    </button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Add POS Coins</DialogTitle>
+                        <DialogDescription>
+                            <PrimaryButton>+</PrimaryButton>
+                            <span>0</span>
+                            <PrimaryButton>-</PrimaryButton>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
