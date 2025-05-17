30789ef0f9dfd634697734dd56e33ffba7d619ee
66zvnc <ivakkkk@icloud.com>
2025-05-17 10:28:32 +0300

    usercredits

---
+++ src/components/UserCredits.tsx
@@ -0,0 +1,8 @@
+export default function UserCredits() {
+    const userCredits= 25;
+    return (
+        <div>
+            <p>{userCredits}</p> <img src="/credits.svg" alt='credits' />
+        </div>
+    );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,21 @@
-import LoyaltyCard from "@/components/LoyaltyCard.tsx";
-import RecentActivity from "@/components/RecentActivity.tsx";
-import {useAppUser} from "@/hooks/auth/useAppUser.ts";
+import Home from "@/components/Home";
 
 function HomePage() {
-    const currDate = new Date().toLocaleDateString("en-US")
-    const {userObject,dbUser} = useAppUser();
-    const visitsThisMonth = 12;
-    console.log(dbUser);
-    return (
-        <div className="container lg:mx-46 md:mx:hidden px-4 sm:px-6 md:px-8 py-6">
-            <h1 className="text-2xl font-semibold mb-1">Hi, {userObject.user?.firstName ?? "Guest"}!</h1>
-            <p className="text-gray-400 mb-4">Today: {currDate} Visits this month: {visitsThisMonth}</p>
-            <div className="border-t-2 border-b-2 border-gray-200 py-4">
-                <LoyaltyCard/>
-            </div>
-            <RecentActivity/>
-        </div>
-    );
+    // const currDate = new Date().toLocaleDateString("en-US")
+    // const {userObject,dbUser} = useAppUser();
+    // const visitsThisMonth = 12;
+    // console.log(dbUser);
+    // return (
+    //     <div className="container lg:mx-46 md:mx:hidden px-4 sm:px-6 md:px-8 py-6">
+    //         <h1 className="text-2xl font-semibold mb-1">Hi, {userObject.user?.firstName ?? "Guest"}!</h1>
+    //         <p className="text-gray-400 mb-4">Today: {currDate} Visits this month: {visitsThisMonth}</p>
+    //         <div className="border-t-2 border-b-2 border-gray-200 py-4">
+    //             <LoyaltyCard/>
+    //         </div>
+    //         <RecentActivity/>
+    //     </div>
+    // );
+    return <Home/>
 }
 
 export default HomePage;
